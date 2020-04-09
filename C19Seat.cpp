a007ae192c092f9101912a11fd23b8d36f4a5d2f
kaik5 <34473749+kaik5@users.noreply.github.com>
2020-04-09 16:50:15 -0700

    Update C19Seat.cpp

--- C19Seat.cpp
+++ C19Seat.cpp
@@ -4,10 +4,10 @@ Author:         Jiajun Kong
 Date:           4/7/2020
 
 Description:    This program uses an algorithm to find an optimal pattern to seat
-                guests in the Coronavirus time, in a n * m theatre where n is the
-                number of rows and m is the number of seats, for social distancing.
-                The seat moves circularly, like Knight's Tour, and it checks the
-                Euclidean, vertical, horizontal distance between two seats. 1 on
+                guests in the Coronavirus time and prints it out. The theather is in a 
+                n * m dimension where n is the number of rows and m is the number of seats.
+                The seat moves circularly, like Knight's Tour, and it checks the Euclidean 
+                vertical, horizontal, diagonal distance between two seats. The value of 1 on
                 the pattern indicate somebody should seat there, 0 otherwise.
 */
 
